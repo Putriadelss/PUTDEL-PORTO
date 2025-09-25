6836a66fa285e9c46c033453fb3c8765bd58c94b
Putri Adelia Suwono <putriadelswn@outlook.com>
2025-09-25 17:02:15 +0700

    Update 2

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function scrollToAbout() {
   document.getElementById("about").scrollIntoView({behavior: "smooth"});
 }
+
+function toggleMenu() {
+  document.getElementById("nav-links").classList.toggle("show");
+}
